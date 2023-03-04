7b27bea7a7f28f996c05a249d2789fd86b44a0a3
AnnaShilova95 <111079982+AnnaShilova95@users.noreply.github.com>
2023-03-04 23:24:26 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+const data =`
+{
+    "results": [
+    {
+        "image": "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/f5bffc5d-71d2-4080-bf4d-364b76a61d10/280x420",
+        "name": "Уолтер Хартвелл Уайт",
+        "actor": "Брайан Крэнстон",
+        "role": "Хайзенберг",
+        "link": [
+            "https://www.kinopoisk.ru/name/57575"
+        ]
+    },
+    {
+        "image": "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/451b3cc7-275f-4c9c-9b05-a8c40a591deb/280x420",
+        "name": "Скайлер Уайт",
+        "actor": "Анна Ганн",
+        "role": "жена Уолтера",
+        "link": [
+            "https://www.kinopoisk.ru/name/9339/"
+        ]
+    }
+]
+}`
